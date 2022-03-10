c8a88190091e868b8950dde73f6b4046e0c8f464
shreevalikushe <shreevalikushe@gmail.com>
2022-03-10 13:20:23 +0530

    Job Location

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,7 +1,7 @@
 export const experienceData = [
   {
     id: 1,
-    company: "X Billion Skills Lab",
+    company: "X Billion Skills Lab, Mumbai",
     jobtitle: "Operations Manager",
     startYear: "2019",
     endYear: "2020",
