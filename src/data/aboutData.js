cac0fac88987765d2ba8b92f04bd3b16fa02052d
shreevalikushe <shreevalikushe@gmail.com>
2022-03-10 17:26:33 +0530

    alignment

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who am I?",
   description1:
-    "My name is Shreevali Kushe, I'm a web developer based in Pune, Maharashtra",
+    "My name is Shreevali Kushe, I'm a web developer based in Pune, Maharashtra.",
   description2:
     "Driven by ambition and fuelled by determination, I am an emerging full stack web developer with a creative interest to build attractive websites. Shaped and moulded by Masai's immersive curriculum, I specialized in MERN stack and now willing to work on and learn from meaningful projects.",
   image: 2,
