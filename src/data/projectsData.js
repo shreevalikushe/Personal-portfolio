11078e070df01985cc675284804442cb33a71d46
shreevalikushe <shreevalikushe@gmail.com>
2022-04-02 18:44:26 +0530

    Added 1 Project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,20 +1,41 @@
 import myntra from "../assets/png/myntra.png";
 import codecov from "../assets/png/codecov.png";
+import makemytrip from "../assets/png/makemytrip.png";
 export const projectsData = [
   {
     id: 1,
-    projectName: "Codecov - Clone",
+    projectName: "MakeMyTrip - Clone",
     projectDesc: [
-      `A clone of Codecov.io website using HTML, CSS and JS. Codecov is the leading, dedicated code coverage solution.`,
-      ["Built Landing page and Resources page", "Website responsiveness"],
+      `A Full stack-MERN project mimicing the user flow and behaviour of MakeMyTrip.com.`,
+      [
+        "Flight and Hotel pages with multiple features",
+        "User Authentication",
+        "Redux integration",
+      ],
     ],
-    tags: ["HTML", "CSS", "Javascript"],
-    code: "https://github.com/shreevalikushe/Codecov_Clone",
-    demo: "https://codecov-clone-website.netlify.app/",
-    image: codecov,
+    tags: ["React", "Redux", "MongoDB", "Express"],
+    code: "https://github.com/shreevalikushe/Make-my-trip-clone",
+    demo: "https://make-my-trip-clone.vercel.app/",
+    image: makemytrip,
   },
   {
     id: 2,
+    projectName: "Myntra - Clone",
+    projectDesc: [
+      `All round Frontend integrated clone mimicing the user flow and behaviour of Myntra.com.`,
+      [
+        "4 product pages with sorting and filters",
+        "Redux integration",
+        "Cart page features",
+      ],
+    ],
+    tags: ["React", "Redux", "MaterialUI"],
+    code: "https://github.com/shreevalikushe/myntra_clone",
+    demo: "https://myntra-clone-gilt.vercel.app/",
+    image: myntra,
+  },
+  {
+    id: 3,
     projectName: "BBC News - Clone",
     projectDesc: [
       `A visual replica of BBC News with user authentication, news search and language translation feature.`,
@@ -31,37 +52,17 @@ export const projectsData = [
       "https://user-images.githubusercontent.com/74674737/146935382-82a4f03d-3f02-4afc-aa02-43c0a21bff1d.png",
   },
   {
-    id: 3,
-    projectName: "Myntra - Clone",
+    id: 4,
+    projectName: "Codecov - Clone",
     projectDesc: [
-      `All round Frontend integrated clone mimicing the user flow and behaviour of Myntra.com.`,
-      [
-        "4 product pages with sorting and filters",
-        "Redux integration",
-        "Cart page features",
-      ],
+      `A clone of Codecov.io website using HTML, CSS and JS. Codecov is the leading, dedicated code coverage solution.`,
+      ["Built Landing page and Resources page", "Website responsiveness"],
     ],
-    tags: ["React", "Redux", "MaterialUI"],
-    code: "https://github.com/shreevalikushe/myntra_clone",
-    demo: "https://myntra-clone-gilt.vercel.app/",
-    image: myntra,
+    tags: ["HTML", "CSS", "Javascript"],
+    code: "https://github.com/shreevalikushe/Codecov_Clone",
+    demo: "https://codecov-clone-website.netlify.app/",
+    image: codecov,
   },
-  // {
-  //   id: 4,
-  //   projectName: "Myntra - Clone",
-  //   projectDesc: [
-  //     `All round Frontend integrated clone mimicing the user flow and behaviour of Myntra.com.`,
-  //     [
-  //       "4 product pages with sorting and filters",
-  //       "Redux integration",
-  //       "Cart page features",
-  //     ],
-  //   ],
-  //   tags: ["React", "Redux", "MaterialUI"],
-  //   code: "https://github.com/shreevalikushe/myntra_clone",
-  //   demo: "https://myntra-clone-gilt.vercel.app/",
-  //   image: myntra,
-  // },
 ];
 
 // Do not remove any fields.
